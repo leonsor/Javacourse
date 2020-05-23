d861061224e442eef94632fdca2abf4758467196
Leon <Leon@Medion>
2020-05-23 23:32:44 +0200

    Week 3 until UniquesIPVisitsOnDay

--- JPALSD/src/week3/LogAnalyzer.java
+++ JPALSD/src/week3/LogAnalyzer.java
@@ -26,10 +26,45 @@ public class LogAnalyzer {
 			System.out.println("Total amount of records: " + records.size()); //TODO delete after testing
 		}
 	}
+	
+	/**
+	 * This method takes the ip-address from each entry, checks if it is already in the 
+	 * ipAddresses ArrayList. If not already in there, add to the list.
+	 * @return amount of unique IPs
+	 */
+	public int countUniqueIPs() {
+		ArrayList<String> ipNumbers = new ArrayList<String>();
+		for(LogEntry log : records) {
+			String ipNumber = log.getIpAddress();
+			if(!ipNumbers.contains(ipNumber)) {
+				ipNumbers.add(ipNumber);
+			}
+		}
+		return ipNumbers.size(); 
+	}
 
 	public void printAll() {
 		for(LogEntry le : records) {
 			System.out.println(le);
+		}
+	}
+	
+	public void printAllHigherThanNumber(int num) {
+		ArrayList<LogEntry> logError = new ArrayList<LogEntry>();
+		for(LogEntry log : records) {
+			int statusCode = log.getStatusCode();
+			if(statusCode > num) {
+				logError.add(log);
+			}
+		}
+		if(logError.size() == 0) {
+			System.out.println("No logs found with status code > " + num);
+		}
+		else {
+			System.out.println("Printing log entries with status code > " + num);
+			for(LogEntry error : logError) {
+				System.out.println(error);
+			}
 		}
 	}
 	
--- JPALSD/src/week3/Tester.java
+++ JPALSD/src/week3/Tester.java
@@ -3,9 +3,7 @@
  */
 package week3;
 
-import java.io.*;
 import java.util.Date;
-
 import edu.duke.*;
 
 /**
@@ -14,16 +12,24 @@ import edu.duke.*;
  */
 public class Tester {
 
+	/**
+	 * Contains tests for the LogEntry Class
+	 */
 	private void testLogEntry() {
 		LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 
 				200, 500);
 		System.out.println(le);
 	}
 	
+	/**
+	 * Contains tests for the LogAnalyzer Class
+	 */
 	private void testLogAnalyzer() {
 		LogAnalyzer lA = new LogAnalyzer();
 		lA.readFile("D:\\Users\\Leon\\Skydrive\\Development\\JPALSD\\data\\Week3\\short-test_log");
 		lA.printAll();
+		System.out.println("Total of unique IP addresses: " + lA.countUniqueIPs());
+		lA.printAllHigherThanNumber(200);
 	}
 	/**
 	 * @param args
