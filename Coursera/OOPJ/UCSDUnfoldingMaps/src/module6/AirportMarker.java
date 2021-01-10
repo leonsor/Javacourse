daf7cb2e9c767eaabd39a805e82a0436dbca203e
leonsor <34835973+leonsor@users.noreply.github.com>
2021-01-10 18:22:51 +0100

    implements labeling of airports when hover
    The showTitle method in AirportMarker is finished.
    Added updated airports_new.dat file with latest copy of internet data
    updated .gitignore and added EarthquakeCityMapSketch

--- Coursera/OOPJ/UCSDUnfoldingMaps/src/module6/AirportMarker.java
+++ Coursera/OOPJ/UCSDUnfoldingMaps/src/module6/AirportMarker.java
@@ -1,10 +1,12 @@
 package module6;
 
+import java.util.HashMap;
 import java.util.List;
 
 import de.fhpotsdam.unfolding.data.Feature;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
+import processing.core.PConstants;
 import processing.core.PGraphics;
 
 /** 
@@ -32,7 +34,28 @@ public class AirportMarker extends CommonMarker {
 
 	@Override
 	public void showTitle(PGraphics pg, float x, float y) {
-		 // show rectangle with title
+		
+		HashMap<String, Object> city = this.getProperties(); //creates a HashMap with values for this Airportmarker [country, altitude, code, City, name]
+		//defines the variables
+		String country = (String) city.get("country");
+		String cityName = (String) city.get("city");
+		String code = (String) city.get("code");
+		if(code.equals ("\\N") ) {
+			code = "no code";
+		}
+		String name = (String) city.get("name");
+		 // show rectangle with output
+		String output = "airport name: " + name + ", " + cityName + ", " + "Country: " + country + ", " + code + ".";
+		System.out.println(output);
+		pg.pushStyle();
+		pg.rectMode(PConstants.CORNER);
+		pg.stroke(110);
+		pg.fill(255,255,255);
+		pg.rect(x, y + 15, pg.textWidth(output) +6, 18, 5);
+		pg.textAlign(PConstants.LEFT, PConstants.TOP);
+		pg.fill(0);
+		pg.text(output, x + 3 , y +18);
+		pg.popStyle();
 		
 		// show routes
 		
